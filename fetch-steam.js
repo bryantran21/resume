a935d789098dc953616e01f91fddd120090c641e
Bryan Tran <bryantran21@gmail.com>
2025-08-31 15:22:46 -0500

    Edited key out of fetch file.

--- fetch-steam.js
+++ fetch-steam.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-const STEAM_API_KEY = 'C3418B52F0983D6FCD8FF95C98D9076C'; // Replace with your actual key
-const STEAM_USER_ID = '76561198096023121'; // Replace with your actual Steam ID
+const { STEAM_API_KEY, STEAM_USER_ID } = process.env;
 
 const steamApiUrl = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${STEAM_API_KEY}&steamid=${STEAM_USER_ID}&include_appinfo=true`;
 
